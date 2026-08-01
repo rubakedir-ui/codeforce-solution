4610e6b71a587ab711414c2b7eb51100a1a8ac34
Ruba Kedir <rubakedir@gmail.com>
2026-08-01 07:52:02 -0700

    Add files via upload

--- B_Also_Try_Minecraft.py
+++ B_Also_Try_Minecraft.py
@@ -1,34 +1,24 @@
-first_line = input().split()
-n = int(first_line[0])
-m = int(first_line[1])
-
-heights = []
-for x in input().split():
-    heights.append(int(x))
-
-pref_lr = [0] * n
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+left = [0] * n
 for i in range(1, n):
-    if heights[i-1] > heights[i]:
-        damage = heights[i-1] - heights[i]
+    if a[i - 1] > a[i]:
+        ans = a[i - 1] - a[i]
     else:
-        damage = 0
-    pref_lr[i] = pref_lr[i-1] + damage
-
-pref_rl = [0] * n
-for i in range(n-2, -1, -1):
-    if heights[i+1] > heights[i]:
-        damage = heights[i+1] - heights[i]
+        ans = 0
+    left[i] = left[i - 1] + ans
+right = [0] * n
+for i in range(n - 2, -1, -1):
+    if a[i + 1] > a[i]:
+        ans = a[i + 1] - a[i]
     else:
-        damage = 0
-    pref_rl[i] = pref_rl[i+1] + damage
+       ans = 0
+    right[i] = right[i + 1] + ans
 for i in range(m):
-    quest = input().split()
-    s = int(quest[0]) - 1 
-    t = int(quest[1]) - 1
-    
+    s, t = map(int, input().split())
+    s = s - 1
+    t = t - 1
     if s < t:
-        ans = pref_lr[t] - pref_lr[s]
-        print(ans)
+        print(left[t] - left[s])
     else:
-        ans = pref_rl[t] - pref_rl[s]
-        print(ans)
+        print(right[t] - right[s])
